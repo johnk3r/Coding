f17c042fe180eb81b0a2f377574b0b7c4c1bc804
johnk3r <johnatan2camargo@gmail.com>
2023-05-17 11:30:24 -0300

    Create Impact_Resource_Hijacking_BitCoin_Wallet.sh

---
+++ HUNT_Simulation/bin/Impact_Resource_Hijacking_BitCoin_Wallet.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo -e "\e[92mCalling bitcoin wallets to download mining toolkits"
+
+curl -s http://pool.minergate.com/dkjdjkjdlsajdkljalsskajdksakjdksajkllalkdjsalkjdsalkjdlkasj  > /dev/null 2>&1
