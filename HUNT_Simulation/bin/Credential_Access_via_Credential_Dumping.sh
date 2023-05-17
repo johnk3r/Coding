949e1b8882312ae7482f832b728a1f8c0da122bb
johnk3r <johnatan2camargo@gmail.com>
2023-05-17 09:33:16 -0300

    Delete Credential_Access_via_Credential_Dumping.sh

--- HUNT_Simulation/bin/Credential_Access_via_Credential_Dumping.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo -e "\e[92mExecuting Credential Access via Credential Dumping. This script is running mimipenguin trying to dump passwords from inside the container environment"
-./bin/mimipenguin/mimipenguin.sh
