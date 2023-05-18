f5064b3349f9fb11e225260cd1164c1e4b1d2ba0
johnk3r <johnatan2camargo@gmail.com>
2023-05-18 12:09:45 -0300

    Create Execution_UserExecution_Generated_EICAR.sh

---
+++ HUNT_Simulation/bin/Execution_UserExecution_Generated_EICAR.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo -e "\e[92mGenerating EICAR files..."
+
+echo -n 'X5O!P%@AP[4\PZX54(P^)7CC)7}\$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!\$H+H*' >/tmp/eicar.com && cp /tmp/eicar.com /tmp/eicar.com.txt && zip -j /tmp/eicar_com.zip /tmp/eicar.com && zip -j /tmp/eicarcom2.zip /tmp/eicar_com.zip
