5161f3c84f39539dfee6c224152df8523672c0ce
johnk3r <johnatan2camargo@gmail.com>
2023-05-20 14:05:37 -0300

    Update Impact_Resource_Hijacking_BitCoin_Wallet.sh

--- HUNT_Simulation/bin/Impact_Resource_Hijacking_BitCoin_Wallet.sh
+++ HUNT_Simulation/bin/Impact_Resource_Hijacking_BitCoin_Wallet.sh
@@ -2,5 +2,5 @@
 
 echo -e "\e[92mCalling bitcoin wallets to download mining toolkits"
 
-curl -s http://pool.minergate.com/dkjdjkjdlsajdkljalsskajdksakjdksajkllalkdjsalkjdsalkjdlkasj  > /dev/null &
-curl -s http://xmr.pool.minergate.com/dhdhjkhdjkhdjkhajkhdjskahhjkhjkahdsjkakjasdhkjahdjk  > /dev/null
+curl -s http://pool.minergate.com/dkjdjkjdlsajdkljalsskajdksakjdksajkllalkdjsalkjdsalkjdlkasj  > /dev/null 2>&1 &
+curl -s http://xmr.pool.minergate.com/dhdhjkhdjkhdjkhajkhdjskahhjkhjkahdsjkakjasdhkjahdjk  > /dev/null 2>&1
