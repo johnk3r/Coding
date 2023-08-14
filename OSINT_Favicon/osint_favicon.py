c4c7ffbfaad54d1c308b84b4b322216a35e1f0a3
johnk3r <johnatan2camargo@gmail.com>
2023-08-14 15:05:16 -0300

    Create osint_favicon.py

---
+++ OSINT_Favicon/osint_favicon.py
@@ -0,0 +1,19 @@
+import requests
+import mmh3
+import base64
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# Solicitar a URL do usuário
+url = input("Digite a URL do ícone de favoritos: ")
+
+# Fazer a solicitação GET
+response = requests.get(url)
+
+# Verificar se a solicitação foi bem-sucedida
+if response.status_code == 200:
+    favicon = base64.encodebytes(response.content)
+    hash_value = mmh3.hash(favicon)
+    print("Hash MurmurHash3 do ícone de favoritos:", hash_value)
+else:
+    print("Não foi possível obter o ícone de favoritos da URL fornecida.")
