835400f4ae66fae278234d01ad204dd13b61196d
johnk3r <johnatan2camargo@gmail.com>
2023-08-15 12:06:41 -0300

    Create check_vpn.py

---
+++ OSINT_VPNIP/check_vpn.py
@@ -0,0 +1,32 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+
+def get_vpn_provider(ip):
+    request = requests.get('https://spur.us/context/{}'.format(ip))
+    soup = BeautifulSoup(request.text, 'html.parser')
+
+    for meta_tag in soup.find_all('meta'):
+        search = re.search('(.*) \( (.*) \) IP Context', str(meta_tag.get('content')))
+        if search:
+            return search.group(2)
+
+def main():
+    input_file = input("[+] Nome do arquivo de IPs: ")
+
+    try:
+        with open(input_file, 'r') as file:
+            ip_list = file.read().splitlines()
+    except FileNotFoundError:
+        print("Arquivo não encontrado.")
+        return
+
+    for ip in ip_list:
+        result = get_vpn_provider(ip)
+        if result:
+            print('IP:', ip, '- Provider:', result)
+        else:
+            print('IP:', ip, '- Não foi possível determinar o provedor.')
+
+if __name__ == "__main__":
+    main()
